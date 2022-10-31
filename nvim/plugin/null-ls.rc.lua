0f164bb4f2132cacc74cf1e45148468987ada5e4
JiantingFeng <13325471722@163.com>
2022-10-31 12:21:01 +0800

    feat: python styling

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -17,10 +17,12 @@ null_ls.setup {
     null_ls.builtins.completion.luasnip,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.completion.spell,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.pycodestyle,
+    null_ls.builtins.formatting.autopep8,
     null_ls.builtins.formatting.stylish_haskell
     -- null_ls.builtins.diagnostics.fish
   },
