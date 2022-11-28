3461e8b71faa7adc65f4309667474114ee0a630e
JiantingFeng <gianting01@gmail.com>
2022-11-28 20:16:54 +0800

    refactor: replace symbolic link

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,17 @@
-/Users/fengjianting/Library/Mobile Documents/com~apple~CloudDocs/Mackup/.config/nvim/init.lua
+-- This is Jianting's nvim configuration entry point
+-- For latest config, please refer https://github.com/JiantingFeng/dotfiles
+require('base')
+require('highlights')
+require('maps')
+require('plugins')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('macos')
+end
+if is_win then
+  require('windows')
+end
