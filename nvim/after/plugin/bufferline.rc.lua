12b33b5418dba930e97e0b10710a3d69911294f0
JiantingFeng <gianting01@gmail.com>
2022-12-02 10:44:48 +0800

    feat(bufferline): change bufferline style

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -4,32 +4,32 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    separator_style = 'thin',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true
   },
-  highlights = {
-    separator = {
-      fg = '#073642',
-      bg = '#002b36',
-    },
-    separator_selected = {
-      fg = '#073642',
-    },
-    background = {
-      fg = '#657b83',
-      bg = '#002b36'
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-    },
-    fill = {
-      bg = '#073642'
-    }
-  },
+  -- highlights = {
+  --   separator = {
+  --     fg = '#073642',
+  --     bg = '#002b36',
+  --   },
+  --   separator_selected = {
+  --     fg = '#073642',
+  --   },
+  --   background = {
+  --     fg = '#657b83',
+  --     bg = '#002b36'
+  --   },
+  --   buffer_selected = {
+  --     fg = '#fdf6e3',
+  --     bold = true,
+  --   },
+  --   fill = {
+  --     bg = '#073642'
+  --   }
+  -- },
 })
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
