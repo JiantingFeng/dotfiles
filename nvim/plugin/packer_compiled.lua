d96c4b037809dafa11596eced1a0a1c5d2813dea
JiantingFeng <gianting01@gmail.com>
2023-01-12 15:35:24 +0800

    Upgrade packages

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -212,11 +212,6 @@ _G.packer_plugins = {
     path = "/Users/fengjianting/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["rust-tools.nvim"] = {
-    loaded = true,
-    path = "/Users/fengjianting/.local/share/nvim/site/pack/packer/start/rust-tools.nvim",
-    url = "https://github.com/simrat39/rust-tools.nvim"
-  },
   ["telescope-file-browser.nvim"] = {
     loaded = true,
     path = "/Users/fengjianting/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
