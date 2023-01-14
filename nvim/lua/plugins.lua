866b83f5af08f7f5a47721aab66bce7db8e95e74
JiantingFeng <gianting01@gmail.com>
2023-01-14 13:03:39 +0800

    feat(lua/plugin.lua): replace packer by lazy

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,71 +1,76 @@
--- Install packer
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-local is_bootstarp = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstarp = true
-  vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-  vim.cmd [[packadd packer.nvim]]
-end
-
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- use {
-  --   'svrana/neosolarized.nvim',
-  --   requires = { 'tjdevries/colorbuddy.nvim' }
-  -- }
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use 'nvim-lualine/lualine.nvim' -- Statusline
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'neovim/nvim-lspconfig' -- LSP
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-
-  use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'L3MON4D3/LuaSnip'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'folke/zen-mode.nvim'
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
+-- Install Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
-  use 'akinsho/nvim-bufferline.lua'
-  -- use 'github/copilot.vim'
+end
+vim.opt.rtp:prepend(lazypath)
 
-  use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
+require('lazy').setup({
+  'wbthomason/packer.nvim',
+  { "catppuccin/nvim", as = "catppuccin" },
+  -- Statusline
+  'nvim-lualine/lualine.nvim',
+  -- Common utilities
+  'nvim-lua/plenary.nvim',
+  'onsails/lspkind-nvim', -- vscode-like pictograms
+  'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
+  'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in LSP
+  'hrsh7th/nvim-cmp', -- Completion
+  'neovim/nvim-lspconfig', -- LSP
+  'jose-elias-alvarez/null-ls.nvim', -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+
+  -- 'glepnir/lspsaga.nvim', -- LSP UIs
+  'L3MON4D3/LuaSnip',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  },
+  'kyazdani42/nvim-web-devicons', -- File icons
+  'nvim-telescope/telescope.nvim',
+  'nvim-telescope/telescope-file-browser.nvim',
+  'windwp/nvim-autopairs',
+  'windwp/nvim-ts-autotag',
+  'norcalli/nvim-colorizer.lua',
+  'folke/zen-mode.nvim',
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  'akinsho/nvim-bufferline.lua',
+  --  'github/copilot.vim'
+
+  'lewis6991/gitsigns.nvim',
+  'dinhhuy258/git.nvim', -- For git blame & browse
   -- vimtex
-  use 'lervag/vimtex'
-  use {
+  'lervag/vimtex',
+  {
     "iurimateus/luasnip-latex-snippets.nvim",
     -- replace "lervag/vimtex" with "nvim-treesitter/nvim-treesitter" if you're
     -- using treesitter.
-    requires = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
+    dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
     config = function()
       require 'luasnip-latex-snippets'.setup()
-      -- or setup({ use_treesitter = true })
+      -- or setup({ _treesitter = true })
     end,
     ft = "tex",
-  }
-  use "numToStr/Comment.nvim"
-  use 'JoosepAlviste/nvim-ts-context-commentstring' -- comment string for js, ts, jsx, astro, lua, etc...
-  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-    require("toggleterm").setup()
-  end } -- floating term
+  },
+  "numToStr/Comment.nvim",
+  'JoosepAlviste/nvim-ts-context-commentstring', -- comment string for js, ts, jsx, astro, lua, etc...
+  {
+    "akinsho/toggleterm.nvim", tag = '*', config = function()
+      require("toggleterm").setup()
+    end
+  }, -- floating term
 
   -- Rust Debugging
-  use 'mfussenegger/nvim-dap'
-end)
+  'mfussenegger/nvim-dap',
+})
