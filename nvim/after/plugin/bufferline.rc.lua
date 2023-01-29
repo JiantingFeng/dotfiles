60ce8c02b5ba17a69bcf85c7c481516734aac4cd
JiantingFeng <gianting01@gmail.com>
2023-01-29 12:52:50 +0800

    Upgrade packages

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -4,11 +4,12 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'thin',
+    separator_style = "slant",
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
+    diagnostics = "nvim_lsp"
   },
   -- highlights = {
   --   separator = {
