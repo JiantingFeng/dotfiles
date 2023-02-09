d53e649fe8fd88a7369ce21def131f64b2b9d893
JiantingFeng <gianting01@gmail.com>
2023-02-09 12:05:25 +0800

    feat: add lualine-lsp-progress

--- nvim/after/plugin/autopairs.rc.lua
+++ nvim/after/plugin/autopairs.rc.lua
@@ -1,23 +1,23 @@
 local status, autopairs = pcall(require, "nvim-autopairs")
 if (not status) then return end
 autopairs.setup({
-  disable_filetype = { "TelescopePrompt", "vim" },
+    disable_filetype = { "TelescopePrompt", "vim" },
 })
 local rule = require("nvim-autopairs.rule")
 local cond = require('nvim-autopairs.conds')
 autopairs.add_rules({
-  rule("$", "$", { "tex", "latex" })
-      -- don't add a pair if the next character is %
-      :with_pair(cond.not_after_regex("%%"))
-      -- don't add a pair if  the previous character is xxx
-      :with_pair(cond.not_before_regex("xxx", 3))
-      -- don't move right when repeat character
-      :with_move(cond.none())
-      -- don't delete if the next character is xx
-      :with_del(cond.not_after_regex("xx"))
-      -- disable adding a newline when you press <cr>
-      :with_cr(cond.none())
+    rule("$", "$", { "tex", "latex" })
+    -- don't add a pair if the next character is %
+    :with_pair(cond.not_after_regex("%%"))
+    -- don't add a pair if  the previous character is xxx
+    :with_pair(cond.not_before_regex("xxx", 3))
+    -- don't move right when repeat character
+    :with_move(cond.none())
+    -- don't delete if the next character is xx
+    :with_del(cond.not_after_regex("xx"))
+    -- disable adding a newline when you press <cr>
+    :with_cr(cond.none())
 },
-  -- disable for .vim files, but it work for another filetypes
-  rule("a", "a", "-vim")
+    -- disable for .vim files, but it work for another filetypes
+    rule("a", "a", "-vim")
 )
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,93 +2,95 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  { "catppuccin/nvim", as = "catppuccin" },
-  -- Statusline
-  'nvim-lualine/lualine.nvim',
-  -- Common utilities
-  'nvim-lua/plenary.nvim',
-  'onsails/lspkind-nvim', -- vscode-like pictograms
-  'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
-  'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in LSP
-  'hrsh7th/nvim-cmp', -- Completion
-  'neovim/nvim-lspconfig', -- LSP
-  'jose-elias-alvarez/null-ls.nvim', -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  'williamboman/mason.nvim', -- Install Lsp
-  'williamboman/mason-lspconfig.nvim',
+    { "catppuccin/nvim", as = "catppuccin" },
+    -- Statusline
+    'nvim-lualine/lualine.nvim',
+    'arkav/lualine-lsp-progress',
+    -- Common utilities
+    'nvim-lua/plenary.nvim',
+    'onsails/lspkind-nvim', -- vscode-like pictograms
+    'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
+    'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in LSP
+    'hrsh7th/nvim-cmp', -- Completion
+    'neovim/nvim-lspconfig', -- LSP
+    'jose-elias-alvarez/null-ls.nvim', -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+    'williamboman/mason.nvim', -- Install Lsp
+    'williamboman/mason-lspconfig.nvim',
 
-  -- 'glepnir/lspsaga.nvim', -- LSP UIs
-  'L3MON4D3/LuaSnip',
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  },
-  'nvim-telescope/telescope.nvim',
-  'nvim-telescope/telescope-file-browser.nvim',
-  'windwp/nvim-autopairs',
-  'windwp/nvim-ts-autotag',
-  'norcalli/nvim-colorizer.lua',
-  'folke/zen-mode.nvim',
-  {
-    "iamcco/markdown-preview.nvim",
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
-  {
-    'akinsho/nvim-bufferline.lua',
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-  },
-  --  'github/copilot.vim'
+    -- 'glepnir/lspsaga.nvim', -- LSP UIs
+    'L3MON4D3/LuaSnip',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    },
+    'nvim-telescope/telescope.nvim',
+    'nvim-telescope/telescope-file-browser.nvim',
+    'windwp/nvim-autopairs',
+    'windwp/nvim-ts-autotag',
+    'norcalli/nvim-colorizer.lua',
+    'folke/zen-mode.nvim',
+    {
+        "iamcco/markdown-preview.nvim",
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+        'akinsho/nvim-bufferline.lua',
+        dependencies = { 'kyazdani42/nvim-web-devicons' },
+    },
+    --  'github/copilot.vim'
 
-  'lewis6991/gitsigns.nvim',
-  'dinhhuy258/git.nvim', -- For git blame & browse
-  -- vimtex
-  'lervag/vimtex',
-  {
-    "iurimateus/luasnip-latex-snippets.nvim",
-    -- replace "lervag/vimtex" with "nvim-treesitter/nvim-treesitter" if you're
-    -- using treesitter.
-    dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
-    config = function()
-      require 'luasnip-latex-snippets'.setup()
-      -- or setup({ _treesitter = true })
-    end,
-    ft = "tex",
-  },
-  "numToStr/Comment.nvim",
-  'JoosepAlviste/nvim-ts-context-commentstring', -- comment string for js, ts, jsx, astro, lua, etc...
-  {
-    "akinsho/toggleterm.nvim", config = function()
+    'lewis6991/gitsigns.nvim',
+    'dinhhuy258/git.nvim', -- For git blame & browse
+    -- vimtex
+    'lervag/vimtex',
+    {
+        "iurimateus/luasnip-latex-snippets.nvim",
+        -- replace "lervag/vimtex" with "nvim-treesitter/nvim-treesitter" if you're
+        -- using treesitter.
+        dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
+        config = function()
+          require 'luasnip-latex-snippets'.setup()
+          -- or setup({ _treesitter = true })
+        end,
+        ft = "tex",
+    },
+    "numToStr/Comment.nvim",
+    'JoosepAlviste/nvim-ts-context-commentstring', -- comment string for js, ts, jsx, astro, lua, etc...
+    {
+        "akinsho/toggleterm.nvim", config = function()
       require("toggleterm").setup()
     end
-  }, -- floating term
+    }, -- floating term
 
-  -- Rust Debugging
-  'mfussenegger/nvim-dap',
-  {
-    'codota/tabnine-nvim',
-    build = './dl_binaries.sh'
-  },
-  -- Diagnostics and Debugging
-  {
-    'folke/trouble.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons' }
-  },
-  -- Which key
-  {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 200
-    end,
-  },
+    -- Rust Debugging
+    'mfussenegger/nvim-dap',
+    {
+        'codota/tabnine-nvim',
+        build = './dl_binaries.sh'
+    },
+    -- Diagnostics and Debugging
+    {
+        'folke/trouble.nvim',
+        dependencies = { 'kyazdani42/nvim-web-devicons' }
+    },
+    -- Which key
+    {
+        "folke/which-key.nvim",
+        config = function()
+          vim.o.timeout = true
+          vim.o.timeoutlen = 200
+        end,
+    },
+
 })
