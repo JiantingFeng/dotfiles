ddf5d9a98a793fdb5da66832ef673c4ebce05bd5
JiantingFeng <gianting01@gmail.com>
2023-02-17 17:16:50 +0800

    Upgrade packages

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/fengjianting/miniconda3/bin/conda
+    eval /Users/fengjianting/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
