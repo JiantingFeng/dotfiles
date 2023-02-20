b4c4344c8432e75329e32a3f0198013cba32e8a7
JiantingFeng <gianting01@gmail.com>
2023-02-20 14:38:18 +0800

    set tuna homebrew mirror for fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,10 @@ if test -f /Users/fengjianting/miniconda3/bin/conda
 end
 # <<< conda initialize <<<
 
+# Set environment variables for Homebrew.
+
+set -x HOMEBREW_API_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+set -x HOMEBREW_BOTTLE_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+set -x HOMEBREW_BREW_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+set -x HOMEBREW_CORE_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+set -x HOMEBREW_PIP_INDEX_URL "https://pypi.tuna.tsinghua.edu.cn/simple"
