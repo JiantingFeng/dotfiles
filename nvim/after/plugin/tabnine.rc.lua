86a1fba30e4e5d977a0bf55cc71c260b68eb5696
JiantingFeng <gianting01@gmail.com>
2023-03-03 11:41:30 +0800

    Upgrade packages

--- nvim/after/plugin/tabnine.rc.lua
+++ nvim/after/plugin/tabnine.rc.lua
@@ -1,7 +1,7 @@
 require('tabnine').setup({
   disable_auto_comment = true,
-  accept_keymap = "<C-]>",
-  dismiss_keymap = "<C-[>",
+  accept_keymap = "<C-.>",
+  dismiss_keymap = "<C-,>",
   debounce_ms = 300,
   suggestion_color = { gui = "#808080", cterm = 244 },
   execlude_filetypes = { "TelescopePrompt" }
